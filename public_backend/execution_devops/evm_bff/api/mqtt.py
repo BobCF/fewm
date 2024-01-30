80004038ccea2da45c98cb07376118db0026a04c
xingyang-007 <xing.yang@intel.com>
2024-01-30 15:39:19 +0800

    add uwsgi and outside-service mqtt

--- public_backend/execution_devops/evm_bff/api/mqtt.py
+++ public_backend/execution_devops/evm_bff/api/mqtt.py
@@ -4,75 +4,90 @@ import json
 import threading
 
 # MQTT broker
-# prod
-broker_address = "121.40.124.59"
+# aliyun rabbitmq prod
+broker_address = "rabbitmq" # outside service inside docker along with rabbitmq
 broker_port = 1883
-user = "mqtt"
-passwd = "mqtt@123"
-# test
-# broker_address = "localhost"
-# broker_port = 1883
-# user = "user"
-# passwd = "password"
+user = "user"
+pw = "password"
 
+# topic for outside-serivce use
+subscribe_topic = "topic/outside-serivce"
 
 '''
-    send mqtt message to topic
+    send mqtt message to inside-service of command
+'''
+def send_command_to_inside_service(data):
+    send_message('topic/inside-service', {
+            'id': str(uuid.uuid1()),
+            'name':'outside-service',
+            'url':'/instructions/command',
+            # 'data': {
+            #     "user": "1",
+            #     "test_cycle_id": "1",
+            #     "test_case_id": "1",
+            #     "test_step_id": "1",
+            # }
+            'data': data,
+        })
+
+'''
+    send mqtt message to asignee when task assigned
+'''
+def send_message_to_asignee(asignee=[], message=""):
+    send_message('topic/mobile/user/<username>', {
+            'id': str(uuid.uuid1()),
+            'name':'inside-service',
+            'url':'/notification/assign',
+            'data': {
+                "message": "task has been assigned to you.",
+                "task": "task name",
+            }
+        })
+
+'''
+    general send mqtt message to topic
 '''
 def send_message(topic, message):
+    # intel proxy forward
+    message['topic'] = topic
+    topic = publish_topic
+
     client = paho_mqtt.Client()
-    client.username_pw_set(user, passwd)
+    client.username_pw_set(user, pw)
     client.connect(broker_address, broker_port)
 
-    info = client.publish(topic, message, qos=1)
-    print(f"[mqtt] Send message to topic={topic}, msg={message}")    
-    # TODO
+    info = client.publish(topic, json.dumps(message), qos=2)
+    print(f"[mqtt] Sent message to topic={topic}, msg={message}")
+    # print(info.is_published())
     info.wait_for_publish(1)
-    print(info.is_published())
     
     client.disconnect()
 
-'''
-    send mqtt message to one user by username
-'''
-def send_message_to_user(username, message):
-    client = paho_mqtt.Client()
-    client.username_pw_set(user, passwd)
-    client.connect(broker_address, broker_port)
-
-    info = client.publish('topic/user/{}'.format(username), message, qos=2)
-    print(info.is_published())
-    # info.wait_for_publish()
-    print(f"[mqtt] Sent message to user={username}, msg={message}")
-    
-    client.disconnect()
-
-'''
-    send mqtt message to all users
-'''
-def send_message_to_all(message):
-    client = paho_mqtt.Client()
-    client.username_pw_set(user, passwd)
-    client.connect(broker_address, broker_port)
-
-    info = client.publish('topic/user/all', message, qos=2)
-    print(f"[mqtt] Sent message to all users, msg={message}")
-    print(info.is_published())
-    # info.wait_for_publish()
-    
-    client.disconnect()
-
-
-# MQTT message handler/router
-def handler(payload):
+"""
+    MQTT message handler
+"""
+def default_handler(payload):
     print(f"Received message: {payload}")
-    # do some script
-    print("some script done")
-    # send message to mobile
-    print("send back message done")
 
+def command_result_handler(payload):
+    print(f"Received message: {payload}")
+    print("send message to mobile user")
+    payload = payload['data']
+    send_message('topic/mobile/user/{user}'.format(payload['user']), 
+        {
+            'id': str(uuid.uuid1()),
+            'name':'outside-service',
+            'url':'/notification/refresh',
+            'data': payload,
+        }
+    )
+
+"""
+    mqtt router
+"""
 mqtt_router = {
-    "/data/flow": handler
+    # /instructions
+    "/instructions/command/result": command_result_handler,
 }
 
 '''
@@ -80,14 +95,14 @@ mqtt_router = {
 '''
 def outside_service_receive_message():
     client = paho_mqtt.Client(client_id='outside-service', clean_session=False)
-    client.username_pw_set(user, passwd)
+    client.username_pw_set(user, pw)
     client.connect(broker_address, broker_port)
 
     def on_message(client, userdata, msg):
         try:
             # decode msg to json, dispatch by url
             payload = json.loads(msg.payload.decode())
-            handler = mqtt_router[payload['url']]
+            handler = mqtt_router.get(payload['url'], default_handler)
             handler(payload)
         except Exception as e:
             print("[error] parse mqtt message: {} {} {}".format(client, userdata, msg))
