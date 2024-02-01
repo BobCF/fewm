00c628961d67a524a5aa442e6263f9ab8d9275ff
xingyang-007 <xing.yang@intel.com>
2024-02-01 16:34:02 +0800

    fix deploy bug

--- public_backend/execution_devops/execution_devops/settings.py
+++ public_backend/execution_devops/execution_devops/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'evm_bff.api.csrf.DisableCSRF',
     'django.middleware.csrf.CsrfViewMiddleware',
     # 'django.contrib.auth.middleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
