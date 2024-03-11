f10ad674e9f2c42e8f2bff5b50a9b86831591c46
xingyang-007 <xing.yang@intel.com>
2024-03-11 15:35:03 +0800

    add tasks filter by status

--- public_backend/execution_devops/evm_bff/views.py
+++ public_backend/execution_devops/evm_bff/views.py
@@ -93,7 +93,14 @@ class TasksView(APIView):
     def get(self,request):
         # query by task fields
         username = request.username
-        logging.debug(request.GET.dict())
-        tasks = Task.objects.filter(assignee=username, **request.GET.dict())
+        tasks = Task.objects.filter(assignee=username)
+        # filter by status
+        params = request.GET.dict()
+        logging.debug(params)
+        if 'status' in params:
+            if params['status'] == 'complete':
+                tasks = tasks.filter(status='complete')
+            else:
+                tasks = Task.exclude(status='complete')
         tasks = [model_to_dict(task) for task in tasks]
         return Response({'code':0,'errmsg':'','data': tasks}, headers=resp_headers)
