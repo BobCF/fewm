4073b263d21240c7d6c56876fecdc2bed5b745f3
xingyang-007 <xing.yang@intel.com>
2024-03-12 15:20:10 +0800

    update log

--- public_backend/execution_devops/execution_devops/settings.py
+++ public_backend/execution_devops/execution_devops/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+import logging
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,8 @@ SECRET_KEY = 'django-insecure-ct=pe^pwhuq(^8z&1rdi@@efofl7iplutd#d0l+n1wsyq!8w1b
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+if DEBUG:
+    logging.basicConfig(level=logging.DEBUG)
 
 # Application definition
 
